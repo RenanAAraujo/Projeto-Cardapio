d1a7c21ac4a0fc9df6cf4bbeb154210b2212a72c
RenanAAraujo <renanarauj5@gmail.com>
2024-03-13 18:56:39 -0300

    Mudança no horario

--- script.js
+++ script.js
@@ -242,7 +242,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 12 && hora <22;
+    return hora >= 18 && hora <22;
     //true = restaurante esta aberto
 }
 
